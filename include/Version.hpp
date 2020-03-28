6bdd7c10741f96b8bb10ddff129980d90f66d7f5
Oscar Rojas <oscar@oscar-rc.dev>
2020-03-28 09:57:40 -0300

    Add Version.hpp

---
+++ include/Version.hpp
@@ -0,0 +1,28 @@
+/*
+	zbnt/server-shared
+	Copyright (C) 2020 Oscar R.
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <cstdint>
+
+extern const char *ZBNT_VERSION;
+extern const uint32_t ZBNT_VERSION_INT;
+extern const uint32_t ZBNT_VERSION_MAJOR;
+extern const uint32_t ZBNT_VERSION_MINOR;
+extern const uint32_t ZBNT_VERSION_PATCH;
+extern const char *ZBNT_VERSION_PREREL;
+extern const char *ZBNT_VERSION_COMMIT;
+extern const bool ZBNT_VERSION_DIRTY;
